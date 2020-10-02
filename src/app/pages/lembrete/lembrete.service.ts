d42772528988b83c4572ea4b9a932dc5648ae0e2
Messias Junior <messiaslima.03@gmail.com>
2020-10-02 18:25:33 +0100

    refactor(lembrete service): remove query params
        - remove query parameters from resource string and add on request options
        - update indent  size to 4 spaces on .editorconfig
        - update package-lock.json (automatically)

--- src/app/pages/lembrete/lembrete.service.ts
+++ src/app/pages/lembrete/lembrete.service.ts
@@ -1,38 +1,72 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Lembrete } from 'src/app/shared/models/lembrete';
-import { AppKeys } from 'src/app/core/keys/app-keys';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable, EventEmitter } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject } from "rxjs";
+
+import { Lembrete } from "src/app/shared/models/lembrete";
+import { AppKeys } from "src/app/core/keys/app-keys";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root",
 })
 export class LembreteService {
+    confirmarExclusao = new EventEmitter();
+    cancelarExclusao = new EventEmitter();
+    edicaoLembrete = new BehaviorSubject(new Lembrete());
+    exibirMensagemBemVindo = new BehaviorSubject(false);
 
-  confirmarExclusao = new EventEmitter();
-  cancelarExclusao = new EventEmitter();
-  edicaoLembrete = new BehaviorSubject(new Lembrete());
-  exibirMensagemBemVindo = new BehaviorSubject(false);
+    constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {}
+    salvarLembrete(lembrete: Lembrete) {
+        return this.http.post<Lembrete>(AppKeys.apiUrl + `lembrete`, lembrete);
+    }
 
-  salvarLembrete(lembrete: Lembrete) {
-    return this.http.post<Lembrete>(AppKeys.apiUrl + `lembrete`, lembrete);
-  }
+    listarLembretes(pagina: number, qtdRegistros: number, pkUsuario: number) {
+        const params = {
+            pagina: pagina.toString(),
+            qtdRegistros: qtdRegistros.toString(),
+            pkUsuario: pkUsuario.toString(),
+        };
 
-  listarLembretes(pagina: number, qtdRegistros: number, pkUsuario: number) {
-    return this.http.get(AppKeys.apiUrl + `lembrete/listar-lembretes?pagina=${pagina}&qtdRegistros=${qtdRegistros}&pkUsuario=${pkUsuario}`);
-  }
+        return this.http.get(AppKeys.apiUrl + `lembrete/listar-lembretes`, {
+            params,
+        });
+    }
 
-  contarLembretesUsuario(pkUsuario: number) {
-    return this.http.get(AppKeys.apiUrl + `lembrete/total-lembretes-usuario?pkUsuario=${pkUsuario}`);
-  }
+    contarLembretesUsuario(pkUsuario: number) {
+        const params = { pkUsuario: pkUsuario.toString() };
 
-  listarLembretesPorTitulo(pagina: number, qtdRegistros: number, titulo: string, pkUsuario: number) {
-    return this.http.get(AppKeys.apiUrl + `lembrete/listar-lembretes-por-titulo?pagina=${pagina}&qtdRegistros=${qtdRegistros}&titulo=${titulo}&pkUsuario=${pkUsuario}`);
-  }
+        return this.http.get(
+            AppKeys.apiUrl + `lembrete/total-lembretes-usuario`,
+            {
+                params,
+            }
+        );
+    }
 
-  excluirLembrete(pkLembrete: number) {
-    return this.http.delete(AppKeys.apiUrl + `lembrete?pkLembrete=${pkLembrete}`);
-  }
+    listarLembretesPorTitulo(
+        pagina: number,
+        qtdRegistros: number,
+        titulo: string,
+        pkUsuario: number
+    ) {
+        const params = {
+            pagina: pagina.toString(),
+            qtdRegistros: qtdRegistros.toString(),
+            titulo: titulo,
+            pkUsuario: pkUsuario.toString(),
+        };
+
+        return this.http.get(
+            AppKeys.apiUrl + `lembrete/listar-lembretes-por-titulo`,
+            { params }
+        );
+    }
+
+    excluirLembrete(pkLembrete: number) {
+        const params = { pkLembrete: pkLembrete.toString() };
+
+        return this.http.delete(AppKeys.apiUrl + `lembrete`, {
+            params,
+        });
+    }
 }
